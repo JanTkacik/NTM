5892c99cd2ae8fa252bf2778367808e460e8f322
Jan Tkacik <jan.tkacik@gmail.com>
2015-02-28 13:34:31 +0100

    Added object pool

---
+++ NeuralTuringMachine/ObjectPool/ObjectArrayPool.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectPool
+{
+    public class ObjectArrayPool<T> where T : PoolableObjectBase
+    {
+        private readonly ObjectPool<T> _basicItemPool; 
+        private readonly Dictionary<int, ObjectPool<PoolableObjectArray<T>>> _objectPools;
+
+        public ObjectArrayPool(Func<T> factory)
+        {
+            _basicItemPool = new ObjectPool<T>(factory);
+            _objectPools = new Dictionary<int, ObjectPool<PoolableObjectArray<T>>>();
+        }
+
+        public T[] GetObjectArray(int length)
+        {
+            if (!_objectPools.ContainsKey(length))
+            {
+                _objectPools.Add(length, new ObjectPool<PoolableObjectArray<T>>(() => new PoolableObjectArray<T>(length, _basicItemPool)));
+            }
+            return _objectPools[length].GetObject().Array;
+        }
+    }
+
+    internal class PoolableObjectArray<T> : PoolableObjectBase  where T : PoolableObjectBase
+    {
+        private readonly T[] _array;
+
+        public PoolableObjectArray(int length, ObjectPool<T> basicObjectPool)
+        {
+            _array = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                _array[i] = basicObjectPool.GetObject();
+            }
+        }
+        
+        public T[] Array
+        {
+            get { return _array; }
+        }
+
+        protected override void OnResetState()
+        {
+            foreach (T item in _array)
+            {
+                item.ResetState();
+            }
+        }
+
+        protected override void OnReleaseResources()
+        {
+            foreach (T item in _array)
+            {
+                item.ReleaseResources();
+            }
+        }
+    }
+}
---
+++ NeuralTuringMachine/ObjectPool/ObjectPool.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace ObjectPool
+{
+    public class ObjectPool<T> where T : PoolableObjectBase
+    {
+        private readonly ConcurrentBag<T> _pooledObject;
+        private readonly Func<T> _factoryMethod;
+
+        public ObjectPool(Func<T> factoryMethod)
+        {
+            _factoryMethod = factoryMethod;
+            _pooledObject = new ConcurrentBag<T>();
+        }
+
+        private void ReturnToPoolAction(PoolableObjectBase pooledObject, bool reRegisterForFinalization)
+        {
+            T item = (T)pooledObject;
+
+            //Reset object state and if reset fails destroy object
+            if (!item.ResetState())
+            {
+                DestroyPooledObject(item);
+                return;
+            }
+
+            //Object resurrection - called from finalize method
+            if (reRegisterForFinalization)
+            {
+                GC.ReRegisterForFinalize(item);
+            }
+
+            _pooledObject.Add(item);
+        }
+
+        private void DestroyPooledObject(PoolableObjectBase objectToDestroy)
+        {
+            if (!objectToDestroy.Disposed)
+            {
+                objectToDestroy.ReleaseResources();
+                objectToDestroy.Disposed = true;
+            }
+
+            GC.SuppressFinalize(objectToDestroy);
+        }
+
+        public T GetObject()
+        {
+            T objectFromPool;
+
+            bool objectAvailible = _pooledObject.TryTake(out objectFromPool);
+            if (objectAvailible)
+            {
+                return objectFromPool;
+            }
+            T newObject = _factoryMethod();
+            newObject.ReturnToPoolAction = ReturnToPoolAction;
+            return newObject;
+        }
+
+        ~ObjectPool()
+        {
+            foreach (T item in _pooledObject)
+            {
+                DestroyPooledObject(item);
+            }
+        }
+    }
+}
