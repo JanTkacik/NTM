2514aa8922ca10f8f6cb38c85d91c543f9d5041f
Jan Tkacik <jan.tkacik@gmail.com>
2015-03-20 11:14:43 +0100

    Continuing refactoring - trainging process

--- NeuralTuringMachine/NTM2/Controller/TrainableNTM.cs
+++ NeuralTuringMachine/NTM2/Controller/TrainableNTM.cs
@@ -1,4 +1,6 @@
-﻿using NTM2.Memory;
+﻿using System;
+using NTM2.Memory;
+using NTM2.Memory.Addressing;
 
 namespace NTM2.Controller
 {
@@ -6,11 +8,20 @@ namespace NTM2.Controller
     {
         private readonly NeuralTuringMachine _controller;
         private readonly MemoryState _memoryState;
+        private readonly ContentAddressing[] _contentAddressings;
+        private readonly HeadSetting[] _headSettings;
+        private readonly ReadData[] _readDatas;
 
-        public TrainableNTM(NeuralTuringMachine controller, MemoryState memoryState)
+        public TrainableNTM(NeuralTuringMachine controller)
         {
             _controller = controller;
-            _memoryState = memoryState;
+
+            //FOREACH HEAD - SET WEIGHTS TO BIAS VALUES
+            _contentAddressings = _controller.Memory.GetContentAddressing();
+            _headSettings = HeadSetting.GetVector(_controller.HeadCount, i => new Tuple<int, ContentAddressing>(_controller.Memory.MemoryColumnsN, _contentAddressings[i]), _controller.UnitFactory);
+            _readDatas = ReadData.GetVector(_controller.HeadCount, i => new Tuple<HeadSetting, NTMMemory>(_headSettings[i], _controller.Memory));
+
+            _memoryState = new MemoryState(_headSettings, _readDatas, _controller.Memory);
         }
 
         public TrainableNTM(TrainableNTM oldMachine, double[] input, UnitFactory unitFactory)
@@ -32,6 +43,20 @@ namespace NTM2.Controller
         {
             _memoryState.BackwardErrorPropagation();
             _controller.BackwardErrorPropagation(knownOutput);
+        }
+
+        public void DataBackwardPropagation()
+        {
+            //Compute gradients for the bias values of internal memory and weights
+            for (int i = 0; i < _readDatas.Length; i++)
+            {
+                _readDatas[i].BackwardErrorPropagation();
+                for (int j = 0; j < _readDatas[i].HeadSetting.Data.Length; j++)
+                {
+                    _contentAddressings[i].Data[j].Gradient += _readDatas[i].HeadSetting.Data[j].Gradient;
+                }
+                _contentAddressings[i].BackwardErrorPropagation();
+            }
         }
     }
 }
--- NeuralTuringMachine/NTM2/Learning/BPTTTeacher.cs
+++ NeuralTuringMachine/NTM2/Learning/BPTTTeacher.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using NTM2.Controller;
-using NTM2.Memory;
-using NTM2.Memory.Addressing;
 
 namespace NTM2.Learning
 {
@@ -20,15 +18,9 @@ namespace NTM2.Learning
 
         public double[][] Train(double[][] input, double[][] knownOutput)
         {
-            //FOREACH HEAD - SET WEIGHTS TO BIAS VALUES
-            ContentAddressing[] contentAddressings = _controller.Memory.GetContentAddressing();
-            HeadSetting[] oldSettings = HeadSetting.GetVector(_controller.HeadCount, i => new Tuple<int, ContentAddressing>(_controller.Memory.MemoryColumnsN, contentAddressings[i]), _controller.UnitFactory);
-            ReadData[] readDatas = ReadData.GetVector(_controller.HeadCount, i => new Tuple<HeadSetting, NTMMemory>(oldSettings[i], _controller.Memory));
-
             TrainableNTM[] machines = new TrainableNTM[input.Length];
-            TrainableNTM empty = new TrainableNTM(_controller, new MemoryState(oldSettings, readDatas, _controller.Memory));
+            TrainableNTM empty = new TrainableNTM(_controller);
 
-            //BPTT
             machines[0] = new TrainableNTM(empty, input[0], _controller.UnitFactory);
 
             for (int i = 1; i < input.Length; i++)
@@ -44,16 +36,7 @@ namespace NTM2.Learning
                 machines[i].BackwardErrorPropagation(knownOutput[i]);
             }
 
-            //Compute gradients for the bias values of internal memory and weights
-            for (int i = 0; i < readDatas.Length; i++)
-            {
-                readDatas[i].BackwardErrorPropagation();
-                for (int j = 0; j < readDatas[i].HeadSetting.Data.Length; j++)
-                {
-                    contentAddressings[i].Data[j].Gradient += readDatas[i].HeadSetting.Data[j].Gradient;
-                }
-                contentAddressings[i].BackwardErrorPropagation();
-            }
+            empty.DataBackwardPropagation();
 
             _weightUpdater.Reset();
             _controller.UpdateWeights(_weightUpdater);
