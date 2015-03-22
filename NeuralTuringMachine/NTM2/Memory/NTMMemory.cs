9b3fa3a2f84b6f5951e13dd36bae25fd66ad6d89
Jan Tkacik <jan.tkacik@gmail.com>
2015-03-22 16:13:00 +0100

    Memory refactoring

--- NeuralTuringMachine/NTM2/Memory/NTMMemory.cs
+++ NeuralTuringMachine/NTM2/Memory/NTMMemory.cs
@@ -41,150 +41,143 @@ namespace NTM2.Memory
             _oldMemory = memory;
             Data = UnitFactory.GetTensor2(memory.CellCountN, memory.CellSizeM);
 
-            int headsCount = heads.Length;
-            _erase = GetTensor2(headsCount, memory.CellSizeM);
-            _add = GetTensor2(headsCount, memory.CellSizeM);
+            _erase = GetTensor2(HeadCount, memory.CellSizeM);
+            _add = GetTensor2(HeadCount, memory.CellSizeM);
             var erasures = GetTensor2(memory.CellCountN, memory.CellSizeM);
 
-            for (int i = 0; i < headsCount; i++)
+            for (int i = 0; i < HeadCount; i++)
             {
                 Unit[] eraseVector = _heads[i].EraseVector;
                 Unit[] addVector = _heads[i].AddVector;
+                double[] erases = _erase[i];
+                double[] adds = _add[i];
 
-                for (int j = 0; j < _erase[i].Length; j++)
+                for (int j = 0; j < CellSizeM; j++)
                 {
-                    _erase[i][j] = Sigmoid.GetValue(eraseVector[j].Value);
-                    _add[i][j] = Sigmoid.GetValue(addVector[j].Value);
+                    erases[j] = Sigmoid.GetValue(eraseVector[j].Value);
+                    adds[j] = Sigmoid.GetValue(addVector[j].Value);
                 }
             }
 
-            for (int i = 0; i < _oldMemory.Data.Length; i++)
+            for (int i = 0; i < CellCountN; i++)
             {
                 Unit[] oldRow = _oldMemory.Data[i];
                 double[] erasure = erasures[i];
                 Unit[] row = Data[i];
-                for (int j = 0; j < oldRow.Length; j++)
+
+                for (int j = 0; j < CellSizeM; j++)
                 {
                     Unit oldCell = oldRow[j];
                     double erase = 1;
                     double add = 0;
-                    for (int k = 0; k < HeadSettings.Length; k++)
+                    for (int k = 0; k < HeadCount; k++)
                     {
                         HeadSetting headSetting = HeadSettings[k];
-                        erase *= (1 - (headSetting.AddressingVector[i].Value*_erase[k][j]));
-                        add += headSetting.AddressingVector[i].Value*_add[k][j];
+                        double addressingValue = headSetting.AddressingVector[i].Value;
+                        erase *= (1 - (addressingValue * _erase[k][j]));
+                        add += addressingValue * _add[k][j];
                     }
                     erasure[j] = erase;
-                    row[j].Value += (erase*oldCell.Value) + add;
+                    row[j].Value += (erase * oldCell.Value) + add;
                 }
             }
-        }
-        
-        private double[][] GetTensor2(int x, int y)
-        {
-            double[][] tensor = new double[x][];
-            for (int i = 0; i < x; i++)
-            {
-                tensor[i] = new double[y];
-            }
-            return tensor;
         }
 
         public void BackwardErrorPropagation()
         {
-            //Gradient of head settings
-            for (int i = 0; i < HeadSettings.Length; i++)
+            for (int i = 0; i < HeadCount; i++)
             {
                 HeadSetting headSetting = HeadSettings[i];
                 double[] erase = _erase[i];
                 double[] add = _add[i];
-                for (int j = 0; j < Data.Length; j++)
-                {
-                    Unit[] row = Data[j];
-                    Unit[] oldRow = _oldMemory.Data[j];
-                    double gradient = 0;
-                    for (int k = 0; k < row.Length; k++)
-                    {
-                        Unit data = row[k];
-                        double oldDataValue = oldRow[k].Value;
-                        for (int q = 0; q < HeadSettings.Length; q++)
-                        {
-                            if (q == i)
-                            {
-                                continue; 
-                            }
-                            HeadSetting setting = HeadSettings[q];
-                            oldDataValue *= (1 - (setting.AddressingVector[j].Value*_erase[q][k]));
-                        }
-                        gradient += ((oldDataValue*(-erase[k])) + add[k])*data.Gradient;
-                    }
-                    headSetting.AddressingVector[j].Gradient += gradient;
-                }
+                Head head = _heads[i];
+
+                HeadSettingGradientUpdate(i, erase, add, headSetting);
+                EraseAndAddGradientUpdate(i, erase, add, headSetting, head);
             }
 
-            //Gradient of Erase vector
-            for (int k = 0; k < _heads.Length; k++)
+            MemoryGradientUpdate();
+        }
+
+        private void MemoryGradientUpdate()
+        {
+            for (int i = 0; i < CellCountN; i++)
             {
-                Head head = _heads[k];
-                Unit[] headErase = head.EraseVector;
-                double[] erase = _erase[k];
-                HeadSetting headSetting = HeadSettings[k];
+                Unit[] oldDataVector = _oldMemory.Data[i];
+                Unit[] newDataVector = Data[i];
 
-                for (int i = 0; i < headErase.Length; i++)
-                {
-                    double gradient = 0;
-                    for (int j = 0; j < Data.Length; j++)
-                    {
-                        Unit[] row = Data[j];
-                        double gradientErase = _oldMemory.Data[j][i].Value;
-                        for (int q = 0; q < HeadSettings.Length; q++)
-                        {
-                            if (q == k)
-                            {
-                                continue;
-                            }
-                            gradientErase *= 1 - (HeadSettings[q].AddressingVector[j].Value * _erase[q][i]);
-                        }
-                        gradient += row[i].Gradient * gradientErase * (-headSetting.AddressingVector[j].Value);
-                    }
-                    double e = erase[i];
-                    head.EraseVector[i].Gradient += gradient * e * (1 - e);
-                }
-            }
-
-            //Gradient of Add vector
-            for (int k = 0; k < _heads.Length; k++)
-            {
-                Head head = _heads[k];
-                double[] add = _add[k];
-                HeadSetting headSetting = HeadSettings[k];
-                Unit[] addVector = head.AddVector;
-
-                for (int i = 0; i < addVector.Length; i++)
-                {
-                    double gradient = 0;
-                    for (int j = 0; j < Data.Length; j++)
-                    {
-                        Unit[] row = Data[j];
-                        gradient += row[i].Gradient*headSetting.AddressingVector[j].Value;
-                    }
-                    double a = add[i];
-                    addVector[i].Gradient += gradient * a * (1 - a);
-                }
-            }
-
-            //Gradient memory
-            for (int i = 0; i < _oldMemory.CellCountN; i++)
-            {
-                for (int j = 0; j < _oldMemory.CellSizeM; j++)
+                for (int j = 0; j < CellSizeM; j++)
                 {
                     double gradient = 1;
-                    for (int q = 0; q < HeadSettings.Length; q++)
+                    for (int q = 0; q < HeadCount; q++)
                     {
                         gradient *= 1 - (HeadSettings[q].AddressingVector[i].Value*_erase[q][j]);
                     }
-                    _oldMemory.Data[i][j].Gradient += gradient*Data[i][j].Gradient;
+                    oldDataVector[j].Gradient += gradient*newDataVector[j].Gradient;
                 }
+            }
+        }
+
+        private void EraseAndAddGradientUpdate(int headIndex, double[] erase, double[] add, HeadSetting headSetting, Head head)
+        {
+            Unit[] addVector = head.AddVector;
+            for (int j = 0; j < CellSizeM; j++)
+            {
+                double gradientErase = 0;
+                double gradientAdd = 0;
+                for (int k = 0; k < CellCountN; k++)
+                {
+                    Unit[] row = Data[k];
+                    double itemGradient = row[j].Gradient;
+                    double addressingVectorItemValue = headSetting.AddressingVector[k].Value;
+
+                    //Gradient of Erase vector
+                    double gradientErase2 = _oldMemory.Data[k][j].Value;
+                    for (int q = 0; q < HeadCount; q++)
+                    {
+                        if (q == headIndex)
+                        {
+                            continue;
+                        }
+                        gradientErase2 *= 1 - (HeadSettings[q].AddressingVector[k].Value * _erase[q][j]);
+                    }
+                    gradientErase += itemGradient * gradientErase2 * (-addressingVectorItemValue);
+                    //Gradient of Add vector
+                    gradientAdd += itemGradient * addressingVectorItemValue;
+                }
+
+                double e = erase[j];
+                head.EraseVector[j].Gradient += gradientErase * e * (1 - e);
+
+                double a = add[j];
+                addVector[j].Gradient += gradientAdd * a * (1 - a);
+            }
+        }
+
+        private void HeadSettingGradientUpdate(int headIndex, double[] erase, double[] add, HeadSetting headSetting)
+        {
+            //Gradient of head settings
+            for (int j = 0; j < CellCountN; j++)
+            {
+                Unit[] row = Data[j];
+                Unit[] oldRow = _oldMemory.Data[j];
+                double gradient = 0;
+                for (int k = 0; k < CellSizeM; k++)
+                {
+                    Unit data = row[k];
+                    double oldDataValue = oldRow[k].Value;
+                    for (int q = 0; q < HeadCount; q++)
+                    {
+                        if (q == headIndex)
+                        {
+                            continue;
+                        }
+                        HeadSetting setting = HeadSettings[q];
+                        oldDataValue *= (1 - (setting.AddressingVector[j].Value * _erase[q][k]));
+                    }
+                    gradient += ((oldDataValue * (-erase[k])) + add[k]) * data.Gradient;
+                }
+                headSetting.AddressingVector[j].Gradient += gradient;
             }
         }
 
@@ -192,7 +185,7 @@ namespace NTM2.Memory
         {
             return ContentAddressing.GetVector(HeadCount, i => _oldSimilarities[i]);
         }
-        
+
         public void UpdateWeights(IWeightUpdater weightUpdater)
         {
             foreach (BetaSimilarity[] betaSimilarities in _oldSimilarities)
@@ -206,6 +199,14 @@ namespace NTM2.Memory
             weightUpdater.UpdateWeight(Data);
         }
 
-        
+        private double[][] GetTensor2(int x, int y)
+        {
+            double[][] tensor = new double[x][];
+            for (int i = 0; i < x; i++)
+            {
+                tensor[i] = new double[y];
+            }
+            return tensor;
+        }
     }
 }
